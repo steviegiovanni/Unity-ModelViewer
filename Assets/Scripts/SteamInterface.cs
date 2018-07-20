4ddb1d0b420c7e22870f561530e47cefcaedc085
Stevie Giovanni <steviegiovanni@gmail.com>
2018-07-20 10:41:57 -0700

    first commit vive branch

---
+++ Assets/Scripts/SteamInterface.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// interfaces Steam input with MultiPartsObject's interaction
+/// </summary>
+public class SteamInterface : MonoBehaviour {
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device device;
+
+	/// <summary>
+	/// the multiparts object component
+	/// </summary>
+	[SerializeField]
+	private MultiPartsObject mpo;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		device = SteamVR_Controller.Input ((int)trackedObject.index);
+		/*if (device.GetHairTriggerDown()) {
+			mpo.ToggleSelect ();
+		}*/
+
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Touchpad)) {
+			mpo.ToggleSelect ();
+		} else if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			mpo.Grab ();
+		} else if (device.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			mpo.Release ();
+		} else if (device.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
+			//mpo.Grab ();
+		} else if (device.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
+			//mpo.Release();
+			mpo.ResetTransform (mpo.Root);
+		}
+	}
+}
