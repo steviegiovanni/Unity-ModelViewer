253a5cd8a1fc4dc340fb44f60c445fdf6ee618a9
MSI\steviegiovanni <steviegiovanni@gmail.com>
2018-08-07 10:52:56 -0700

    added hololens stuff

---
+++ Assets/Scripts/Hololens/HololensRayObject.cs
@@ -0,0 +1,13 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HololensRayObject : MonoBehaviour, IHasRay {
+    public GazeStabilizer raySource;
+
+    public Ray GetRay()
+    {
+        return raySource.StableRay;
+    }
+}
