6e87dd7b76de845cda8a720193253b9744e086a1
MSI\steviegiovanni <steviegiovanni@gmail.com>
2018-08-31 08:33:36 -0700

    continue cherrypick from hololens branch

--- Assets/Scripts/Hololens/HololensInterface.cs
+++ Assets/Scripts/Hololens/HololensInterface.cs
@@ -8,41 +8,46 @@ using UnityEngine.SceneManagement;
 
 public class HololensInterface : MonoBehaviour, IInputClickHandler {
     public TaskList TaskList;
+    public MultiPartsObject MPO;
+
+    public void CleanUpHololensPrefabs()
+    {
+        var go = GameObject.Find("MixedRealityCameraParent");
+        if (go != null)
+            Destroy(go);
+        go = GameObject.Find("DefaultCursor");
+        if (go != null)
+            Destroy(go);
+        go = GameObject.Find("InputManager");
+        if (go != null)
+            Destroy(go);
+    }
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
         Debug.Log(eventData.selectedObject.name);
-        if(eventData.selectedObject.name == "Disassembly")
+        if(eventData.selectedObject.name == "TimingBelt")
         {
-            var go = FindObjectOfType<AnimatedCursor>();
-            if (go != null)
-                Destroy(go.gameObject);
-            var go1 = FindObjectOfType<MotionControllerVisualizer>();
-            if (go1 != null)
-                Destroy(go1.gameObject);
-            var go2 = FindObjectOfType<MixedRealityCameraManager>();
-            if (go2 != null)
-                Destroy(go2.gameObject);
-            var go3 = FindObjectOfType<FocusManager>();
-            if (go3 != null)
-                Destroy(go3.gameObject);
-            var go4 = FindObjectOfType<StabilizationPlaneModifier>();
-            if (go4 != null)
-                Destroy(go4.gameObject);
-            var go5 = FindObjectOfType<InputManager>();
-            if (go5 != null)
-                Destroy(go5.gameObject);
-            var go6 = FindObjectOfType<GazeManager>();
-            if (go6 != null)
-                Destroy(go6.gameObject);
-            SceneManager.LoadScene("DynamicContent-hololens-disassembly");
+            CleanUpHololensPrefabs();
+            SceneManager.LoadScene("DynamicContent-hololens-timingbelt");
         }
-        else if (eventData.selectedObject.name == "ToggleGrab")
+        else if (eventData.selectedObject.name == "HeadGasket")
         {
+            CleanUpHololensPrefabs();
+            SceneManager.LoadScene("DynamicContent-hololens-headgasket");
+        }
+        else if (eventData.selectedObject.name == "Inspect")
+        {
+            CleanUpHololensPrefabs();
+            SceneManager.LoadScene("DynamicContent-hololens-inspect");
         }
         else if(eventData.selectedObject.name == "Reset")
         {
             TaskList.Reset();
+        }
+        else if (eventData.selectedObject.name == "ResetMPO")
+        {
+            MPO.ResetAll(MPO.Root);
         }
     }
 
